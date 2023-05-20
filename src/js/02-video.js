5b763344170cb472c462432b033702f241996be8
SvetlanaDobro <122154093+SvetlanaDobro@users.noreply.github.com>
2023-05-20 20:41:58 +0300

    task-2-3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+const VimeoPlayer = require('@vimeo/player');
+const throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+    
+const storedTime = localStorage.getItem('videoplayer-current-time');
+
+if (storedTime) {
+  player.setCurrentTime(storedTime);
+}
+
+const updateStoredTime = throttle(() => {
+  player.getCurrentTime().then((currentTime) => {
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  });
+}, 1000);
+
+player.on('timeupdate', updateStoredTime);
+   
+
+
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,63 @@
+const throttle = require('lodash.throttle');
 
+const STORAGE_KEY = 'feedback-form-state';
+
+let formData = {};
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea')
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', throttle(onMessageInput, 500));
+refs.email.addEventListener('input', throttle(onEmailInput, 500));
+
+refs.form.addEventListener('input', evt => {
+  formData[evt.target.name] = evt.target.value;
+  saveFormData();
+});
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  clearFormData();
+}
+
+function onMessageInput(evt) {
+  const message = evt.target.value;
+  formData.message = message;
+  saveFormData();
+}
+
+function onEmailInput(evt) {
+  const email = evt.target.value;
+  formData.email = email;
+  saveFormData();
+}
+
+function saveFormData() {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function loadFormData() {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  if (savedData) {
+    formData = JSON.parse(savedData);
+    if (formData.email) {
+      refs.email.value = formData.email;
+    }
+    if (formData.message) {
+      refs.textarea.value = formData.message;
+    }
+  }
+}
+
+function clearFormData() {
+  formData = {};
+  localStorage.removeItem(STORAGE_KEY);
+  refs.form.reset();
+}
+
+loadFormData();
+console.log(formData)
